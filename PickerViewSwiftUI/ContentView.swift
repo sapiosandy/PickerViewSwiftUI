4466207f346870c02715e87f38846872de63956d
Sandy Gomez <sgomez1029@gmail.com>
2024-02-19 14:27:40 -0800

    Initial Commit

---
+++ PickerViewSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PickerViewSwiftUI
+//
+//  Created by Sandra Gomez on 2/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PickerViewSwiftUI/PickerViewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickerViewSwiftUIApp.swift
+//  PickerViewSwiftUI
+//
+//  Created by Sandra Gomez on 2/19/24.
+//
+
+import SwiftUI
+
+@main
+struct PickerViewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
